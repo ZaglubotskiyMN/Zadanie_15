e222b7d4d3bb97c79a19abbea4950774cbbe7df9
ZaglubotskiyMN <zaglubotskii@gmail.com>
2021-02-19 21:39:15 +0400

    finally commite

--- src/part1/lessons15/task1/Main.java
+++ src/part1/lessons15/task1/Main.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class Main {
 
 
-    public static void main(String[] args)  {
-        List<person> person = new ArrayList<person>();
+    public static void main(String[] args) {
+        List<person> Listperson = new ArrayList<person>();
         person one = new person("Ivan", 20);
         person two = new person("Petr", 25);
         person three = new person("Elena", 19);
@@ -18,26 +18,33 @@ public class Main {
         person six = new person("Ivan", 40);
         person seven = new person("Ivan", 40);
 
-        person.add(one);
-        person.add(two);
-        person.add(three);
-        person.add(four);
-        person.add(five);
-        person.add(six);
-        person.add(seven);
+        Listperson.add(one);
+        Listperson.add(two);
+        Listperson.add(three);
+        Listperson.add(four);
+        Listperson.add(five);
+        Listperson.add(six);
+        Listperson.add(seven);
 
-        //Collections.sort(person);
-        //System.out.println("not sorter");
-        for (person p : person)
+
+        System.out.println("Без сортировки");
+        for (person p : Listperson)
             System.out.println(p);
-
-
-
+        System.out.println("----------------");
+        System.out.println("Сортировка по имени");
+        Collections.sort(Listperson, new SortedByName());
+        for (person p : Listperson)
+            System.out.println(p.toString());
+        System.out.println("----------------");
+        System.out.println("Сортировка по возрасту");
+        Collections.sort(Listperson, new SortedByAge());
+        for (person p : Listperson)
+            System.out.println(p.toString());
 
 
     }
 
-    public static class person  {
+    public static class person {
         private String name;
         private int age;
         String temp = "name=%s,age=%d";
@@ -69,40 +76,30 @@ public class Main {
             this.age = age;
         }
 
-        class sortedbyname implements Comparable<person> {
-            @Override
-            public int compareTo(person p) {
-                // return getName().compareTo(p.getName());
-                if (getName() == p.getName())
-                    return 1;
-                else return -1;
 
+    }
 
-            }
-
+    static class SortedByName implements Comparator<person> {
+        @Override
+        public int compare(person p1, person p2) {
+            String str1 = p1.getName();
+            String str2 = p2.getName();
+            return str1.compareTo(str2);
         }
+    }
 
-
-
-
-
-       /* @Override
-        public int compareTo(Object obj) {
-
-            person good = (person)obj;
-
-            int res=name.compareTo(good.name);
-            if(res!=0)
-                return res;
-
-            res = age-good.age;
-            if(res!=0)
-                return res;
-            return 0;
-        }*/
-
-
-
-}
-
+    static class SortedByAge implements Comparator<person> {
+        @Override
+        public int compare(person p1, person p2) {
+            int age1 = p1.getAge();
+            int age2 = p2.getAge();
+            if (age1 > age2) {
+                return 1;
+            } else if (age1 < age2) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
 }
